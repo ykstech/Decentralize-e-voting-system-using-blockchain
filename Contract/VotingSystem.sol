bd4dc2b34193cf9b796a93c9cf8ef10cd509dbc1
Yash Kumar Sugandhi <85982995+ykstech@users.noreply.github.com>
2023-10-06 23:43:24 +0530

    Update VotingSystem.sol

--- Contract/VotingSystem.sol
+++ Contract/VotingSystem.sol
@@ -11,24 +11,19 @@ mapping(address => Candidate) private candidates;
 constructor() {
 owner = msg.sender;
 }
-function addCandidate(string memory _name, address
- _candidateAddress) public {
- require(msg.sender == owner, "Only the owner can 
- perform this action.");
+function addCandidate(string memory _name, address_candidateAddress) public {
+ require(msg.sender == owner, "Only the owner can perform this action.");
 candidates[_candidateAddress] = Candidate(_name, 0);
 }
 function vote(address _candidateAddress) public {
-require(!hasVoted[msg.sender], "You have already 
- voted.");
+require(!hasVoted[msg.sender], "You have already voted.");
 hasVoted[msg.sender] = true;
 candidates[_candidateAddress].voteCount++;
 }
 function getData(address _candidateAddress) public view
  returns(string memory,uint){
-require(msg.sender == owner, "Only the owner can 
- perform this action.");
-return(candidates[_candidateAddress].name,
- candidates[_candidateAddress].voteCount);
+require(msg.sender == owner, "Only the owner can perform this action.");
+return(candidates[_candidateAddress].name,candidates[_candidateAddress].voteCount);
 }
 }
 
